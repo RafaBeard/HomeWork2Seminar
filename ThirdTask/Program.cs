13a5b5d1f1007a25ca03860effebd02ee9677290
TRI <rafael.tagirzyanov@yandex.ru>
2022-08-27 16:43:33 +0300

    Третья задача

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num == 6 || Num == 7)
+{
+    Console.WriteLine("Да, это выходной день.");
+}
+else
+{
+    Console.WriteLine("Нет, это невыходной день.");
+}
